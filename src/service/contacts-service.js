e86e7fcc050a9affa4adf4057e876f566a975d08
Bohdan Yevsiienko <vad99992@gmail.com>
2024-02-02 21:46:32 +0200

    start 7hw

---
+++ src/service/contacts-service.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://65bccc66b51f9b29e9324e62.mockapi.io/contacts';
+
+export const getContacts = async () => {
+  const result = await axios.get('/contacts');
+  return result.data;
+};
+
+export const createContact = async data => {
+  const result = await axios.post('/contacts', data);
+  return result.data;
+};
+
+export const deleteContactApi = async id => {
+  const result = await axios.delete(`/contacts/${id}`);
+  return result.data;
+};
